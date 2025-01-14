fa4cda293a60a3b3b5120ae40e82db8be96b5706
Joshua Trujillo <jtriley15@gmail.com>
2025-01-14 17:39:25 +0000

    nit, ordering

--- scripts/fuzz-test.sh
+++ scripts/fuzz-test.sh
@@ -6,14 +6,14 @@ set -e
 # - `nargo`
 # - `cargo`
 
-# export noir functions as-is
-nargo export
-
-# run rust prop tests against noir functions
-cargo test
-
 # export noir functions purely in brillig
 nargo export --force-brillig
 
 # run rust prop tests against brillig functions
+cargo test
+
+# export noir functions as-is
+nargo export
+
+# run rust prop tests against noir functions
 cargo test
