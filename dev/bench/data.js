41b7d5ce408e6c1a0722ddb2918311112c064cb7
github-action-benchmark <github@users.noreply.github.com>
2025-12-03 17:10:34 +0000

    add Brillig Bytecode Size (customSmallerIsBetter) benchmark result for 815d44db19d275d76d349c551e28a597ae507dcf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764781831930,
+  "lastUpdate": 1764781834296,
   "repoUrl": "https://github.com/noir-lang/sha256",
   "entries": {
     "ACIR Opcodes": [
@@ -203,6 +203,50 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/noir-lang/sha256/commit/303f182b2d3696809a4c34a082ce36d01d2a4ca2"
         },
         "date": 1764773591707,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "test_sha256_1",
+            "value": 782,
+            "unit": "opcodes"
+          },
+          {
+            "name": "test_sha256_200",
+            "value": 986,
+            "unit": "opcodes"
+          },
+          {
+            "name": "test_sha256_511",
+            "value": 1297,
+            "unit": "opcodes"
+          },
+          {
+            "name": "test_sha256_512",
+            "value": 1298,
+            "unit": "opcodes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "815d44db19d275d76d349c551e28a597ae507dcf",
+          "message": "feat: remove overflow checks when reconstructing `msg_item` (#46)",
+          "timestamp": "2025-12-03T17:10:04Z",
+          "tree_id": "0cf4e3d0c115a93328d51595af7b1fd120802782",
+          "url": "https://github.com/noir-lang/sha256/commit/815d44db19d275d76d349c551e28a597ae507dcf"
+        },
+        "date": 1764781834281,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
