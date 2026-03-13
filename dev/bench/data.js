431b8dd730d25f7a25073e6cbabc40a7a34ba787
github-action-benchmark <github@users.noreply.github.com>
2026-03-13 17:55:09 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for e20d7275a2533b209a04503cbe62c14b4d995121

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765297341796,
+  "lastUpdate": 1773424509505,
   "repoUrl": "https://github.com/noir-lang/sha256",
   "entries": {
     "ACIR Opcodes": [
@@ -463,6 +463,50 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/noir-lang/sha256/commit/9442e5b6856f98b2ec029882d7e90199ecff91ba"
         },
         "date": 1765297338630,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "test_sha256_1.json/main",
+            "value": 500,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_sha256_200.json/main",
+            "value": 1662,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_sha256_511.json/main",
+            "value": 3185,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_sha256_512.json/main",
+            "value": 3181,
+            "unit": "acir_opcodes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e20d7275a2533b209a04503cbe62c14b4d995121",
+          "message": "chore: remove unnecessary `mut` keyword (#58)",
+          "timestamp": "2026-03-13T17:54:45Z",
+          "tree_id": "95f8ccafc0ddbd78db5d8cc354e7e1b24bf501d8",
+          "url": "https://github.com/noir-lang/sha256/commit/e20d7275a2533b209a04503cbe62c14b4d995121"
+        },
+        "date": 1773424508739,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
